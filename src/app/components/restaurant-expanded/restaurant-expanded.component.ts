33373479d6a255b0a0f85eeb77ab3457f79cf31a
Matt Pua <matthew.pua@slalom.com>
2018-01-16 08:55:23 -0500

    add in food menus

--- src/app/components/restaurant-expanded/restaurant-expanded.component.ts
+++ src/app/components/restaurant-expanded/restaurant-expanded.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, EventEmitter, Output} from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output, OnChanges} from '@angular/core';
 import { Restaurant } from '../../classes/winterlicious';
 
 @Component({
@@ -6,12 +6,18 @@ import { Restaurant } from '../../classes/winterlicious';
   templateUrl: './restaurant-expanded.component.html',
   styleUrls: ['./restaurant-expanded.component.scss']
 })
-export class RestaurantExpandedComponent implements OnInit {
+export class RestaurantExpandedComponent implements OnInit, OnChanges {
   @Input() restaurant: Restaurant;
   @Output() resetSelectedRestaurant: EventEmitter<void> = new EventEmitter();
+
+  activeMenu: string;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngOnChanges() {
+    this.activeMenu = (this.restaurant.lic_lunchmenu.length ? 'lunch' : (this.restaurant.lic_dinnermenu.length ? 'dinner' : ''));
   }
 
   get reviews() {
