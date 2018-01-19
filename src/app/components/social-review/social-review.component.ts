d9072ce7d9cc9b5f2d22c5882a10831121f7713b
Matt Pua <matthew.pua@slalom.com>
2018-01-18 23:57:46 -0500

    weird bug where not able to get the class types for the reviews when on github pages

--- src/app/components/social-review/social-review.component.ts
+++ src/app/components/social-review/social-review.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { YelpReview, GoogleReview } from '../../classes/winterlicious';
+import { YelpReview, GoogleReview, GoogleData } from '../../classes/winterlicious';
 import * as moment from 'moment';
 @Component({
   selector: 'app-social-review',
@@ -28,11 +28,13 @@ export class SocialReviewComponent implements OnInit {
   }
 
   get isYelpReview(): boolean {
-    return this.review.constructor.name === 'YelpReview';
+    return (this.review as YelpReview).id && (this.review as YelpReview).id.length > 0;
+    // return this.review.constructor.name === 'YelpReview';
   }
 
   get isGoogleReview(): boolean {
-    return this.review.constructor.name === 'GoogleReview';
+    return (this.review as GoogleReview).authorUrl && (this.review as GoogleReview).authorUrl.length > 0;
+    // return this.review.constructor.name === 'GoogleReview';
   }
 
   get url(): string {
